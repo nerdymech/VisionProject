b8c315f8fde52eb9af04008ca43833e88db6f590
msit18 <msit@wellesley.edu>
2014-11-05 05:02:30 -0500

    Update webcam_modified_fixed.py
    Switched if else statement so it stops printing "There are no faces" when there are faces.  Trying to get a window to show cropped_face image.  Print statements added to help with debugging.  Moved pdb statements to help with flow and debugging.

--- src/webcam_modified_fixed.py
+++ src/webcam_modified_fixed.py
@@ -30,8 +30,11 @@ def detectFaces():
             minNeighbors=10,
             minSize=(30, 30)
         )
-        if len(faces) != 0:
-
+        
+        if len(faces) == 0:
+            print "There are no faces."
+            
+        else:
             # Draw a rectangle around the faces
             for (x,y,w,h) in faces:
                 print "I see %s people" %len(faces)
@@ -41,8 +44,13 @@ def detectFaces():
                 # I don't know how/why this next line works
                 # global roi_gray
                 roi_gray = gray[y:y+h, x:x+w]
-                upperbound = y+ 2*(h/3) 
-                cropped_face = roi_gray[upperbound:y+h, x:x+w]
+                x_upperbound = x+ 2*(w/3)
+                y_upperbound = y+ 2*(h/3) 
+                print y_upperbound
+                print y+h
+                print x_upperbound
+                print x+w
+                cropped_face = roi_gray[y_upperbound:y+h, x_upperbound:x+w]
                 #roi_color = frame[y:y+h, x:x+w]
                 #print "here"
                 #code to run smile detection (since smiles are on faces)
@@ -59,18 +67,22 @@ def detectFaces():
                     # global roi_gray2
                     roi_gray2 = gray[y+sy:y+sh, x+sx:x+sw]
 
-                    import pdb
-                    pdb.set_trace()
+                    # import pdb
+                    # pdb.set_trace()
 
                 # Display the resulting frame
-            else:
-                print "There are no faces."
+            # else:
+            #     print "There are no faces."
             
             cv2.imshow('Video', frame)
             cv2.imshow('ROI', roi_gray)
+            # cv2.imshow('Cropped face', cropped_face)
         
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 pass
+            
+            import pdb
+            pdb.set_trace()
 
     # When everything is done, release the capture
     video_capture.release()
