90042e8eb4ecc9a4f7cd36b79ebdb852eed9ca03
msit18 <msit@wellesley.edu>
2014-11-06 15:29:33 -0500

    Update load_smiles.py
    Cleaned up code a bit.  Formatting mostly and changed print statement

--- src/smiles/load_smiles.py
+++ src/smiles/load_smiles.py
@@ -3,19 +3,15 @@ from sklearn.datasets import *
 from scipy.io import loadmat
 import numpy 
 
-
- 
 def load_smiles():
 	smiles = loadmat('smile_dataset.mat')
 	return_val = sklearn.datasets.base.Bunch()
 	return_val.data = smiles['X']
 	return_val.target = smiles['expressions']
-	print "smiles %s" %smiles
-	print "finished!"
+	# print "smiles %s" %smiles
+	# print "hi"
 	return return_val
     
-
-
 if __name__ == '__main__':
     load_smiles()
 
