22a73a3336a6cf0916b4f91c06b51a6dbc638ab8
msit18 <msit@wellesley.edu>
2014-11-06 15:28:09 -0500

    Update train_smile.py
    Cleaned up code, turned into method and created main method.  Works with the webcam_modified_fixed.py code

--- src/smiles/train_smile.py
+++ src/smiles/train_smile.py
@@ -1,19 +1,22 @@
 from load_smiles import *
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
+from pickle import dump
 import numpy
-from numpy import *
+# from numpy import *
 
-data = load_smiles()
-model = LogisticRegression()
-print model
-model.fit(data.data, data.target)
-
-#import pdb
-#pdb.set_trace()
-
-#fig = plt.figure()
-
-plt.matshow(numpy.reshape(model.raw_coef_[0][1:],(24,24)).transpose(),cmap='gray')
-
-plt.show()
+def train_smiles ():
+  data = load_smiles()
+  model = LogisticRegression()
+  #could also do SVC (Support Vector Machines) instead of LogisticRegression
+  model.fit(data.data, data.target)
+  #print model.score(data.data, data.target)
+  
+  # plt.matshow(numpy.reshape(model.raw_coef_[0][1:],(24,24)).transpose(),cmap='gray')
+  
+  # plt.show()
+  
+  return model
+  
+if __name__ == '__main__':
+	model = train_smiles()
